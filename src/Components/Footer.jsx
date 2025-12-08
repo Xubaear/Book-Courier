f346be48e86679c91d1c0fce2d8e7d5705e61f7d
Xubaear <xubaear@gmail.com>
2025-12-08 16:25:42 +0600

    added navbar

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    
+    <div className='mt-10'>
     <footer className="footer sm:footer-horizontal bg-base-300 text-base-content p-10">
   <nav>
     <h6 className="footer-title">Services</h6>
@@ -53,9 +53,16 @@ const Footer = () => {
             d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
         </svg>
       </a>
+      
     </div>
+    
   </nav>
+  
+
 </footer>
+
+
+</div>
   );
 };
 
--- src/Layout/HomeLayout.jsx
+++ src/Layout/HomeLayout.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Footer from '../Components/Footer';
 import { Outlet } from 'react-router';
-
+import Navbar from '../Components/Navbar'
 const HomeLayout = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
